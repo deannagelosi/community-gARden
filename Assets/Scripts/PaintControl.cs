f269de42bb4bad43783b67325e51228659863720
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2021-11-29 14:09:00 -0800

    tracking user left hand in unity

--- Assets/Scripts/PaintControl.cs
+++ Assets/Scripts/PaintControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class paint : MonoBehaviour
+public class PaintControl : MonoBehaviour
 {
     public Transform baseDot;
     public KeyCode mouseLeft;
